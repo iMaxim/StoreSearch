db9dfdfe33b29c2ba08cc039899c6c17a928dfaa
Maxim Prosvirkin <maxim.prosvirkin@gmail.com>
2020-08-14 00:38:56 +0300

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Maxim Prosvirkin on 14.08.2020.
+//  Copyright © 2020 Maxim Prosvirkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
