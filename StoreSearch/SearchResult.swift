522079a3b899eb340549e5db06a2d33535c4eddf
Maxim Prosvirkin <maxim.prosvirkin@gmail.com>
2020-08-14 16:15:16 +0300

    Added data model for searched results, added tableview extension

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,14 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Maxim Prosvirkin on 14.08.2020.
+//  Copyright © 2020 Maxim Prosvirkin. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
