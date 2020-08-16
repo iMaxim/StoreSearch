c0a7fa5b8557856f0a776e7605a7bcf6d7459d24
Maxim Prosvirkin <maxim.prosvirkin@gmail.com>
2020-08-16 03:35:52 +0300

    fixed typo

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -34,7 +34,7 @@ class SearchViewController: UIViewController {
         cellNib = UINib(nibName: TableView.CellIdentifiers.nothingFoundCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.nothingFoundCell)
         
-        searchBar.becomeFirstResponder()
+        searchBar.becomeFirstResponder()
     }
 }
 
